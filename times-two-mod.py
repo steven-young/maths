cfad83d1a4610573ccd1e3b5f975d4b45f3ab804
Steven Young <steven.young@it.ox.ac.uk>
2026-02-16 23:46:18 +0000

    Further analysis, added -c option to times-two-mod.py

--- times-two-mod.py
+++ times-two-mod.py
@@ -6,6 +6,7 @@ def main():
 	parser = argparse.ArgumentParser(description="Print num times two sequence mod n")
 	parser.add_argument("number", type=int, help="Number")
 	parser.add_argument("modulus", type=int, help="Modulus")
+	parser.add_argument("-c","--show_count",action="store_true",help="Show count of modulo sequence")
 	args = parser.parse_args()
 	num = args.number
 	mod = args.modulus
@@ -16,7 +17,9 @@ def main():
 		print(f"{cur} ",end='')
 		cur = (cur*2)%mod
 		count += 1
-	print(f"count={count}")
+	if (args.show_count):
+		print(f"count={count}",end='')
+	print('')
 
 if __name__ == "__main__":
 	main()
