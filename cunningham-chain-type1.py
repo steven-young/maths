d6976e2ba74a4302d2ab4dffe6c6e73deca04865
Steven Young <steven.young@it.ox.ac.uk>
2026-02-19 08:21:36 +0000

    Work done on 210.list

--- cunningham-chain-type1.py
+++ cunningham-chain-type1.py
@@ -2,6 +2,8 @@
 import argparse
 import math
 
+cont_count = 20
+
 def is_prime(n):
 	if n <= 1:
 		return False
@@ -49,42 +51,77 @@ def prime_factors(n: int) -> list[int]:
 #			val += 1
 #	return val
 
-def main():
-	parser = argparse.ArgumentParser(description="Print Cunningham chain of type 1.")
-	parser.add_argument("number", type=int, help="The start of the chain")
-	parser.add_argument("-c","--show_composite",action="store_true",help="Print composite value at end of chain")
-	parser.add_argument("-l","--show_length",action="store_true",help="Print length of chain")
-	parser.add_argument("-n","--report_not_start",action="store_true",help="Report if chain doesn't start with number")
-	args = parser.parse_args()
-	num = args.number
+def cc1(num, args):
+	global cont_count
 	start = args.number
-	not_start = False
 	if (is_prime(num)):
 		prev = (num-1)/2.0
+		reported_start = False
 		while (prev >= 2 and int(prev) == prev):
 			if (is_prime(prev)):
-				not_start = True
+				if (args.report_not_start and not reported_start):
+					print(f"{start} not start of chain: ", end='', flush=True)
+					reported_start = True
 				num=int(prev)
 				prev = (num-1)/2.0
 			else:
 				break
 		length = 0
 		while (is_prime(num)):
-			print(f"{num} ", end='')
+			print(f"{num} ", end='', flush=True)
 			num = 2*num+1
 			length += 1
 		else:
-			if (args.show_composite):
-				factors=prime_factors(num)
-				print(f"({num}="+"*".join(str(i) for i in factors)+") ", end='')
 			if (args.show_length):
-				print(f"Chain length {length} ", end='')
-			if (not_start and args.report_not_start):
-				print(f"{start} not start of chain ", end='')
-		print('')
+				print(f"Chain length {length} ", end='', flush=True)
+			if (args.show_end):
+				factors=prime_factors(num)
+				print(f"({num}="+"*".join(str(i) for i in factors)+") ", end='', flush=True)
+			if (args.continuous):
+				if (cont_count > 0 or args.cont_count == 0):
+					print("(", end='', flush=True)
+					while (not is_prime(num)):
+						factors=prime_factors(num)
+						print(f"{num}="+"*".join(str(i) for i in factors)+" ", end='', flush=True)
+						num = 2*num+1
+						if (args.continuous):
+							cont_count = cont_count - 1
+					print(f"{num} is next prime) ", end='', flush=True)
+		print('', flush=True)
 	else:
 		factors=prime_factors(num)
-		print(f"{num} is composite "+"*".join(str(i) for i in factors))
+		print(f"{num} is composite "+"*".join(str(i) for i in factors)+" ",end='', flush=True)
+		if (args.continuous):
+			if (cont_count > 0 or args.cont_count == 0):
+				num = 2*num+1
+				while (not is_prime(num)):
+					factors=prime_factors(num)
+					print(f"{num}="+"*".join(str(i) for i in factors)+" ", end='', flush=True)
+					num = 2*num+1
+					if (args.continuous):
+						cont_count = cont_count - 1
+				print(f"{num} is next prime) ", end='', flush=True)
+		print('', flush=True)
+	return num
+
+def main():
+	global cont_count
+	parser = argparse.ArgumentParser(description="Print Cunningham chain of type 1.")
+	parser.add_argument("number", type=int, help="The start of the chain")
+	parser.add_argument("-e","--show_end",action="store_true",help="Print composite value at end of chain")
+	parser.add_argument("-l","--show_length",action="store_true",help="Print length of chain")
+	parser.add_argument("-n","--report_not_start",action="store_true",help="Report if chain doesn't start with number")
+	parser.add_argument("-c","--continuous",action="store_true",help="Continue along the chain for a few more steps")
+	parser.add_argument("--cont_count",type=int,default=20,help="Count of how long to continue...")
+	args = parser.parse_args()
+	num = args.number
+	if (args.continuous):
+		if (args.cont_count != 0):
+			cont_count = args.cont_count
+		while (cont_count > 0 or args.cont_count == 0):
+			num = cc1(num, args)
+	else:
+		num = cc1(num, args)
 
 if __name__ == "__main__":
 	main()
